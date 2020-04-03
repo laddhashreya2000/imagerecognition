8e5c0c96a0ea4d50fe6230b8088eb2a8a267e9ac
Shreya Laddha <47536876+laddhashreya2000@users.noreply.github.com>
2020-04-03 23:02:03 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,50 +1,28 @@
 # Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
 #no preprocessing. It is done manually by making seperate folders.
-# Part 1 - Building the CNN
 
-# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-# Initialising the CNN
 classifier = Sequential()
-
-# Step 1 - Convolution
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 #3 in input shape for no. of channels in image = here rgb so 3.
 #activation fn used so that no negative pixel value and for non-linearity.
-# Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Adding a second convolutional layer to get lesser difference in training and test accuracy.
+# Adding a second convolutional layer to get better accuracy.
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
-# Step 3 - Flattening
 classifier.add(Flatten())
-
-# Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
 #power of 2 preferable and by experiment around 100 should be the value.
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
-
-# Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Part 2 - Fitting the CNN to the images
 #image augmentation
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -70,3 +48,15 @@ classifier.fit_generator(training_set,
                          epochs = 10,
                          validation_data = test_set,
                          validation_steps = 2000)#equal to size of test set
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)#this will make image as 3d array. 
+test_image = np.expand_dims(test_image, axis = 0)#we need 4 d array for conv2d it corresponds to the batch
+result = classifier.predict(test_image)
+training_set.class_indices#tells us whether 1 is dog
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
